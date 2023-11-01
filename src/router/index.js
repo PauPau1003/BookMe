ca3463bc10b268dd2f710e27a8507934353ce8f2
TeckXuan <khooteckxuan@gmail.com>
2023-11-01 16:36:16 +0800

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,80 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import { auth } from '../firebase/firebaseconfig.js'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import { auth } from "../firebase/firebaseconfig.js";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
-      meta:{
-        requiresAuth:true
-      }
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      meta:{
-        requiresAuth:true
-      }
+      component: () => import("../views/AboutView.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/service-provider',
-      name: 'service-provider',
+      path: "/service-provider",
+      name: "service-provider",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ServiceProvider.vue'),
-      meta:{
-        requiresAuth:true
-      }
+      component: () => import("../views/ServiceProvider.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/search-result',
-      name: 'search-result',
+      path: "/search-result",
+      name: "search-result",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SearchResult.vue'),
-      meta:{
-        requiresAuth:true
-      }
+      component: () => import("../views/SearchResult.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
-      path: '/loginPage',
-      name: 'LoginPage', // eslint-disable-next-line
-      component: () => import('../views/LoginPage.vue') 
+      path: "/loginPage",
+      name: "LoginPage", // eslint-disable-next-line
+      component: () => import("../views/LoginPage.vue"),
     },
-   
-    
-  ]
-})
+
+    {
+      path: "/HomeLandingPage",
+      name: "HomeLandingPage",
+      component: () => import("../views/HomelandingPage.vue"),
+    },
+  ],
+});
 
 router.beforeEach((to, from, next) => {
-  if (to.path === '/loginPage' && auth.currentUser) {
-   
-    next('/') //want to redirect to home, return 
+  if (to.path === "/loginPage" && auth.currentUser) {
+    next("/"); //want to redirect to home, return
     return;
   } //from login to home, where next you want to go to
 
-  if (to.matched.some(record => record.meta.requiresAuth) && !auth.currentUser) {
-    next('/loginPage')//need to modify this because we do not want the user to redirect if they are not logged in
+  if (
+    to.matched.some((record) => record.meta.requiresAuth) &&
+    !auth.currentUser
+  ) {
+    next("/loginPage"); //need to modify this because we do not want the user to redirect if they are not logged in
 
     return;
   }
 
-  next(); 
-})
+  next();
+});
 
-export default router
+export default router;
