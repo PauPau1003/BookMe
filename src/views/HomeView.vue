25faa1df31314f1b7ffd2b23b049ab0c737e128e
zanzzxc <gxlee.2022@scis.smu.edu.sg>
2023-11-01 15:41:36 +0800

    f23efw

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,7 @@ import TheWelcome from '../components/TheWelcome.vue'
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
     <router-link to="/service-provider">Service Provider</router-link>
-    <router-link to="/Dashboard">Dashboard</router-link>
     <router-link to="/SPHome">SPHome</router-link>
-    <router-link to="/LearningAvenue">LearningAvenue</router-link>
-    <router-link to="/Permits&Requirements">Permits&Requirements</router-link>  
     <button @click="$store.dispatch('logout')">Logout</button>
   </div>
   <main>
