70afdbb762814a60d6c344435dd9ba88b6ee6ee7
Wai Horng Liew <whliew.2022@scis.smu.edu.sg>
2023-11-06 15:32:56 +0800

    fixed login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,103 +1,109 @@
 <template>
-<div class="bg">
-<div class="container min-height-container">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-2"></div>
-            <div class="col-lg-6 col-md-8 login-box">
-                <div class="col-lg-12 login-title">
-                    <img src="/BookMeLogo-removebg-preview.png" alt="Logo" style="height: 100px;">
-                </div>
-
-                <div class="col-lg-12 login-form">
+    <div class="bg">
+    <div class="container min-height-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-2"></div>
+                <div class="col-lg-6 col-md-8 login-box">
+                    <div class="col-lg-12 login-title">
+                        <img src="/BookMeLogo-removebg-preview.png" alt="Logo" style="height: 100px;">
+                    </div>
+    
                     <div class="col-lg-12 login-form">
-                       
-                            <ul class="nav nav-tabs justify-content-center nav-pills nav-fill" id="loginRegisterTabs" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link active custom-tab" id="login-tab" data-bs-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true">Login</a>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link custom-tab" id="register-tab" data-bs-toggle="tab" href="#register" role="tab" aria-controls="register" aria-selected="false">Register</a>
-                            </li>
-                            </ul>
-
-                            <div class="tab-content mt-3" id="loginRegisterTabContent">
-                                <!-- Login Tab Content -->
-                                <div class="tab-pane show active" id="login" role="tabpanel" aria-labelledby="login-tab">
-                                <!-- Login Form -->
-                                <form class="login" @submit.prevent="login">
-                                    <div class="mb-3">  
-                                    <label for="loginEmail" class="form-label">Email address</label>
-                                    <input required v-model="login_form.email" type="email" class="form-control" id="loginEmail" placeholder="Enter email">
+                        <div class="col-lg-12 login-form">
+                           
+                                <ul class="nav nav-tabs justify-content-center nav-pills nav-fill" id="loginRegisterTabs" role="tablist">
+                                <li class="nav-item" role="presentation">
+                                    <a class="nav-link active custom-tab" id="login-tab" data-bs-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true" @click="activateLoginTab">Login</a>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <a class="nav-link custom-tab" id="register-tab" data-bs-toggle="tab" href="#register" role="tab" aria-controls="register" aria-selected="false" @click="activateRegisterTab">Register</a>
+                                </li>
+                                </ul>
+    
+                                <div class="tab-content mt-3" id="loginRegisterTabContent">
+                                    <!-- Login Tab Content -->
+                                    <div class="tab-pane show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+                                    <!-- Login Form -->
+                                    <form class="login" @submit.prevent="login">
+                                        <div class="mb-3">  
+                                        <label for="loginEmail" class="form-label">Email address</label>
+                                        <input required v-model="login_form.email" type="email" class="form-control" id="loginEmail" placeholder="Enter email">
+                                        </div>
+                                        <div class="mb-3">
+                                        <label for="loginPassword" class="form-label">Password</label>
+                                        <input required v-model="login_form.password" type="password" class="form-control" id="loginPassword" placeholder="Password">
+                                        </div>
+                                        <button type="submit" value="login" class="btn btn-outline-primary">Login</button>
+                                    </form>
+                                    </div>
+                                </div>
+    
+                                <!-- Register Tab Content -->
+                                <div class="tab-pane" id="register" role="tabpanel1" aria-labelledby="register-tab">
+                                <!-- Registration Form -->
+                                <form class="register" @submit.prevent="register" v-if="!loginTabActive">
+                                    <div class="mb-3">
+                                    <label for="registerEmail" class="form-label">Email address</label>
+                                    <input required type="email" v-model="register_form.email" class="form-control" id="registerEmail" placeholder="Enter email">
                                     </div>
                                     <div class="mb-3">
-                                    <label for="loginPassword" class="form-label">Password</label>
-                                    <input required v-model="login_form.password" type="password" class="form-control" id="loginPassword" placeholder="Password">
+                                    <label for="registerPassword" class="form-label">Password</label>
+                                    <input required v-model="register_form.password" type="password" class="form-control" id="registerPassword" placeholder="Password">
                                     </div>
-                                    <button type="submit" value="login" class="btn btn-outline-primary">Login</button>
+                                    <button type="submit" value="login" class="btn btn-outline-primary">Register</button>
                                 </form>
                                 </div>
-                            </div>
-
-                            <!-- Register Tab Content -->
-                            <div class="tab-pane" id="register" role="tabpanel1" aria-labelledby="register-tab">
-                            <!-- Registration Form -->
-                            <form class="register" @submit.prevent="register">
-                                <div class="mb-3">
-                                <label for="registerEmail" class="form-label">Email address</label>
-                                <input required type="email" v-model="register_form.email" class="form-control" id="registerEmail" placeholder="Enter email">
-                                </div>
-                                <div class="mb-3">
-                                <label for="registerPassword" class="form-label">Password</label>
-                                <input required v-model="register_form.password" type="password" class="form-control" id="registerPassword" placeholder="Password">
-                                </div>
-                                <button type="submit" value="login" class="btn btn-outline-primary">Register</button>
-                            </form>
-                            </div>
-
-                       
+    
+                           
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-</div>
 </template>
-  
-<script>
-import { ref } from 'vue';
-import { useStore } from 'vuex';
+      
+    <script>
+    import { ref } from 'vue';
+    import { useStore } from 'vuex';
+        
+    export default {
+    setup() {
+        const login_form = ref({});
+        const register_form = ref({});
+        const store = useStore();
+        const loginTabActive = ref(true);
     
-export default {
-setup() {
-    const login_form = ref({});
-    const register_form = ref({});
-    const store = useStore();
-    const registerTabActive = ref(false);
-
-    const login = () => {
-    store.dispatch('login', login_form.value);
+        const login = () => {
+        store.dispatch('login', login_form.value);
+        };
+    
+        const register = () => {
+        store.dispatch('register', register_form.value);
+        };
+    
+        const activateRegisterTab = () => {
+        loginTabActive.value = false;
+        };
+    
+        const activateLoginTab = () => {
+        loginTabActive.value = true;
+        };
+    
+        return {
+        login_form,
+        register_form,
+        login,
+        register,
+        loginTabActive,
+        activateRegisterTab,
+        activateLoginTab
+        };
+    }
     };
-
-    const register = () => {
-    store.dispatch('register', register_form.value);
-    };
-
-    const activateRegisterTab = () => {
-    registerTabActive.value = true;
-    };
-
-    return {
-    login_form,
-    register_form,
-    login,
-    register,
-    registerTabActive
-    };
-}
-};
-  </script>
+    </script>
 
 
 <style>
